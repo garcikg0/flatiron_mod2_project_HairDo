55686fdd71ecca3d8fc2ba035a2949444f6aaa21
Valkryn <calvinraphaelg@gmail.com>
2020-07-28 13:38:55 -0400

    added basic layout
    layout

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,2 +1,28 @@
 class StylistsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  
 end
