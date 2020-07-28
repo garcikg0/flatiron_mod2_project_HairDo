f9158a02958e96587f5dee2f9c0b422cb9ef9f97
garcikg0 <kg.gar91@gmail.com>
2020-07-28 10:38:30 -0400

    testing again

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#KG test
-#CG test
-#CG test 2
+#KG random 
