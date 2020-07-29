a2d662054435df17557ee3bd67320f1334e74105
garcikg0 <kg.gar91@gmail.com>
2020-07-29 12:57:21 -0400

    CG's home page and KG review form and show done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :client
   belongs_to :stylist
+  validates :rating, inclusion: { within: 1...5}, presence: true
+  validates :description , presence: true , length: { minimum: 10 }
 end
