f55e259a27648250ff0c51207a7bf7c0dd0bf6e8
Valkryn <calvinraphaelg@gmail.com>
2020-07-29 01:59:04 -0400

    syntax and form links
    -Fixed table syntax
    -Added client edit profile
    -Added Become a Stylist link to connect to new stylist form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,20 +1,30 @@
 class ClientsController < ApplicationController
-    
+
     def index
         @clients = Client.all
     end
-    
+
     def show
         @client = Client.find(params[:id])
     end
-    
+
     def new
         @client = Client.new
     end
 
-    def create 
+    def create
         client = Client.create(client_params)
         redirect_to client
+    end
+
+    def edit
+      @client = Client.find(params[:id])
+    end
+
+    def update
+      client = Client.find(params[:id])
+      client.update(client_params)
+      redirect_to client_path(client)
     end
 
     private
