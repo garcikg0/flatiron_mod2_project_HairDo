c789e0628cd4bb29ddb43fbe85cc73a2d285b1c1
garcikg0 <kg.gar91@gmail.com>
2020-07-30 16:34:27 -0400

    helper methods in AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_stylist 
+    helper_method :current_client
+
+    def current_stylist
+        session[:stylist] ||= []
+    end
+
+    def current_client
+        session[:client] ||= []
+    end
 end
