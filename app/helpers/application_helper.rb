c3b57119f0cefa5e29cf0d6892ba6b5bc858a92b
Valkryn <calvinraphaelg@gmail.com>
2020-07-30 18:04:26 -0400

    boolean on log_in and log_out

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+# module ApplicationHelper
+#
+#   def current_client
+#     @current_client = Client.find(session[:user_id]) if session[:user_id]
+#   end
+#
+#   def logged_in?
+#     !!current_client
+#   end
+#
+#
+# end
