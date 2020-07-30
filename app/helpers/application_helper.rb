842c6ed0076bb2d5a48cb576747125308b78e098
Valkryn <calvinraphaelg@gmail.com>
2020-07-30 18:31:07 -0400

    application contoller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
-# module ApplicationHelper
-#
-#   def current_client
-#     @current_client = Client.find(session[:user_id]) if session[:user_id]
-#   end
-#
-#   def logged_in?
-#     !!current_client
-#   end
-#
-#
-# end
+module ApplicationHelper
+
+  def current_client
+    @current_client = Client.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_client
+  end
+
+
+end
